ab79d382be621c158c74ef48975e605f6b480791
iokim <iokim1220@gmail.com>
2024-02-08 23:02:07 +0900

    Initial commit

---
+++ src/main/java/com/allonsy/altorithmjava/AltorithmJavaApplication.java
@@ -0,0 +1,13 @@
+package com.allonsy.altorithmjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltorithmJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AltorithmJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allonsy/altorithmjava/AltorithmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allonsy.altorithmjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltorithmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
